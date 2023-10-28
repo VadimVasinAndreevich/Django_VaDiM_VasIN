fe947a041469180646430c6cd4c33490bd83fefb
VadimVasinAndreevich <132100792+VadimVasinAndreevich@users.noreply.github.com>
2023-10-28 10:24:14 +0500

    Add files via upload

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'myapp',
+    'myapp2',
 ]
 
 MIDDLEWARE = [
@@ -80,7 +81,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.db',
     }
 }
 

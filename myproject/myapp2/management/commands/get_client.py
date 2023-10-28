1d1429f4080730899e9c47b09211bdd50063a5c8
VadimVasinAndreevich <132100792+VadimVasinAndreevich@users.noreply.github.com>
2023-10-28 10:34:07 +0500

    Update and rename get_user.py to get_client.py

--- myproject/myapp2/management/commands/get_client.py
+++ myproject/myapp2/management/commands/get_client.py
@@ -3,12 +3,12 @@ from myapp2.models import Client
 
 
 class Command(BaseCommand):
-    help = "Get user by id."
+    help = "Get client by id."
 
     def add_arguments(self, parser):
-        parser.add_argument('pk', type=int, help='User ID')
+        parser.add_argument('pk', type=int, help='Client ID')
 
     def handle(self, *args, **kwargs):
         pk = kwargs['pk']
-        user = Client.objects.filter(pk=pk).first()
-        self.stdout.write(f'{user}')
+        client = Client.objects.filter(pk=pk).first()
+        self.stdout.write(f'{client}')
